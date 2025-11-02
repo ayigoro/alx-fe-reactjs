c3938149063f1e96617f0497ee0eef92e2c6435f
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-11-02 16:56:46 +0100

    Added UserProfile Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
@@ -47,6 +48,10 @@ function App() {
 
       <div>
         <Footer />
+      </div>
+
+      <div>
+        <UserProfile name="Alice"  age="25"  bio="Loves hiking and photography"/>
       </div>
     </>
   )
