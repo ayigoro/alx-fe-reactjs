0bb818ac08639469506c6b4572b2af2e2c455f7b
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-11-02 16:18:18 +0100

    Added WelcomMessage component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
        <div>
-        <WelcomeMessage/>
+        <WelcomeMessage />
       </div>
     </>
   )
