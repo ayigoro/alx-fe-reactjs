e873587fbd58aabeccbc4e4c9f1de2a9982092b9
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-11-08 12:07:55 +0100

    Added alx-react-app-new file

---
+++ alx-react-app-new/src/components/styled.jsx
@@ -0,0 +1,12 @@
+const styling = {color:"blue", fontSize: "35px", backgroundColor:"lightgray", width:"600px", margin:"auto", borderRadius:"10px"}
+
+
+function Styled (){
+  return (
+    <div>
+      <h1 style={styling}>Welcom to my React js</h1>
+    </div>
+  )
+}
+
+export default Styled
