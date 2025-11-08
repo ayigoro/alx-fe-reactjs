e5b4194909623bf7b76f27f7606de82f2281948d
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-11-08 22:39:37 +0100

    Added Home,Contact,Services and About components

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import React from 'react'
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Services from "./Services";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar" style={{display :'flex', justifyContent:'space-around', gap :'20px'}}>
+      <Link to = "/">Home</Link>
+      <Link to = "/about">About</Link>
+      <Link to = "/contact">Contact</Link>
+      <Link to = "/services">Services</Link>
+    </nav>
+  )
+}
+
+export default Navbar
