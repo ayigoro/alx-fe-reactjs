c37dc533d4b18fefd35d4789c195f41cc9b7ced5
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-11-16 21:32:00 +0100

    Added RecipeList and AddRecipeForm components

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -2,36 +2,35 @@ import { useState } from "react";
 import useRecipeStore from "./recipeStore";
 
 
-const AddRecipeForm = () => {
-  const addRecipe = useRecipeStore(state => state.addRecipe)
-  const [title,setTitle] = useState('')
-  const [description, setDescription] = useState('')
+ const AddRecipeForm = () => {
+    const addRecipe = useRecipeStore(state => state.addRecipe);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
-  const handleSumbit = (event) =>{
-    event.preventDefault()
-    addRecipe:({id:Date.now(),title,description})
-    setTitle('')
-    setDescription('')
-  }
-  return (
-    <form onSubmit={handleSumbit}>
-      <input 
-      type="text"
-      value={title} 
-      onChange={(e) => setDescription(e.target.value)}
-      placeholder="Description"
-      />
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      addRecipe({ id: Date.now(), title, description });
+      setTitle('');
+      setDescription('');
+    };
 
-      <textarea
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <textarea
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           placeholder="Description"
         />
+        <button type="submit">Add Recipe</button>
+      </form>
+    );
+  };
 
-      <button type="submit">Add Recipe</button>
 
-    </form>
-  )
-}
-
-export default AddRecipeForm
+  export default AddRecipeForm
