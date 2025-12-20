51495fbc8668904fc5cb9f8329272ff3698c0f3f
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-12-20 15:09:15 +0100

    Added github-user-search file

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -6,13 +6,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <Router>
+      <Home/>
+      <About/>
+      <Contact/>
       <Routes>
         <Route path = "/" element = {<Home/>}/>
         <Route path= "/about"  element = {<About/>}/>
         <Route path= "/contact"  element = {<Contact/>}/>                               
       </Routes>
     </Router>
+    </BrowserRouter>
   )
 }
 
