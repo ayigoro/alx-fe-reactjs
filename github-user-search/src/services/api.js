cf061ad77b7956cf070704fa1972f68d5d9cc52a
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-12-22 21:37:10 +0100

    all files added

--- github-user-search/src/services/api.js
+++ github-user-search/src/services/api.js
@@ -4,9 +4,15 @@ import React from 'react'
 
 
 
-const fetchData = async () =>{
-  const response = await fetch ("https://api.example.com/data")
-  return response.json()
+const GITHUB_API_KEY = import.meta.env.VITE_APP_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
+const fetchRepo = async ()=> {
+  const response = await fetch ("https://api.github.com/user/repos", {
+    headers:{
+      Authorization : 'token ${GITHUB_API_KEY}',
+    },
+  })
+    return response.json()
 }
+
 
 export default api
